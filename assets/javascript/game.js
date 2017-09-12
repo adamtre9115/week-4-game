39a31d81e75d60dbb0a62c78b831bdc951a68b58
adamtre9115 <treadam9115@gmail.com>
2017-09-11 20:35:09 -0400

    add skeleton html and variables to gameJS

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var numMatch = $("#randNum"); //number for user to match
+var wins = $("#wins");
+var loss = $("#losses");
+var diamond = $("#diamond") //diamond image
+var ruby = $("#ruby"); //ruby image
+var emerald = $("#emerald"); //emerald image
+var sapphire = $("#sapphire"); //sapphire image
+var userScore = $("#userScore");
+
+// generate random number between 19 and 120 for mumber to guess
+
+// generate random number between 1 and 12 for each image 
+
+// as user clicks crystals add points to user score
+
+// if user score matches number to guess generate win
+
+// if user score is over number to guess lose
+
+// reset game/new game function
