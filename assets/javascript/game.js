2b0cdda02b97d16f097c6734b69680bba74c0f92
adamtre9115 <treadam9115@gmail.com>
2017-09-12 18:33:10 -0400

    update logic with scoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var numMatch ; //number for user to match
 var wins = 0;
 var loss = 0;
+var turnCount = 15;
 var diamond ; //diamond image
 var ruby ; //ruby image
 var emerald ; //emerald image
@@ -11,38 +12,63 @@ var userScore = 0;
 $(document).ready(function(){
 
     // generate random number between 19 and 120 for mumber to guess
+    // generate random number between 1 and 12 for each image 
      function getRandInt(max, min) {
         return Math.floor(max - Math.random()*(max-min));
       }
       
-      numMatch = getRandInt(120, 19);
-      $("#randNum").html(numMatch);
-    // generate random number between 1 and 12 for each image 
-      diamond = getRandInt(12, 1);
-      ruby = getRandInt(12, 1);
-      emerald = getRandInt(12, 1);
-      sapphire = getRandInt(12, 1);
+    
+
     // as user clicks crystals add points to user score
-      $("#diamond").on("click", function(){
-          userScore += diamond;
-          $("#userScore").html(userScore); 
-      })
-      $("#ruby").on("click", function(){
-        userScore += ruby;
-        $("#userScore").html(userScore); 
-    })
-    $("#emerald").on("click", function(){
-        userScore += emerald;
-        $("#userScore").html(userScore); 
-    })
-    $("#sapphire").on("click", function(){
-        userScore += sapphire;
-        $("#userScore").html(userScore); 
-    })
-    // if user score matches number to guess generate win
-
-    // if user score is over number to guess lose
-
+      updateNums();
+        function updateNums(){
+            // update spaces with randomly generated numbers
+            numMatch = getRandInt(120, 19);
+            $("#randNum").html(numMatch);
+            diamond = getRandInt(12, 1);
+            ruby = getRandInt(12, 1);
+            emerald = getRandInt(12, 1);
+            sapphire = getRandInt(12, 1);
+            $("#turns").html(turnCount);
+            $("#diamond").on("click", function(){
+                userScore += diamond;
+                turnCount--;
+                $("#userScore").html(userScore); 
+                $("#turns").html(turnCount);
+            })
+            $("#ruby").on("click", function(){
+              userScore += ruby;
+              turnCount--;
+              $("#userScore").html(userScore); 
+              $("#turns").html(turnCount);
+          })
+          $("#emerald").on("click", function(){
+              userScore += emerald;
+              turnCount--;
+              $("#userScore").html(userScore); 
+              $("#turns").html(turnCount);
+          })
+          $("#sapphire").on("click", function(){
+              userScore += sapphire;
+              turnCount--;
+              $("#userScore").html(userScore);
+              $("#turns").html(turnCount); 
+          })
+        }  
+    
+        // if user score matches number to guess generate win
+        winLose();
+        function winLose(){
+            if (turnCount < 1 && userScore === numMatch){
+                alert("You win!");
+                wins++;
+                $("#wins").html(wins);
+            // if user score is over or less than number to guess user loses
+            }else if (turnCount < 1 && userScore < numMatch || turnCount < 1 && userScore > numMatch) {
+                loss++;
+                $("#losses").html(loss);
+            }
+        }
     // reset game/new game function
 
 });
